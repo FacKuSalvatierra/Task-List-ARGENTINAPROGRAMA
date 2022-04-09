77225e19bbcade9958d809bebf46a8c6045b8b16
Facundo <facku995@gmail.com>
2022-04-08 21:44:33 -0300

    task list listo

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtomComponent } from './components/buttom/buttom.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes:Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtomComponent,
+    TasksComponent,
+    TasksItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing : true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from 'src/app/Task';
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+
+  constructor(
+    private TaskService : TaskService
+  ) { }
+
+  ngOnInit(): void {
+    this.TaskService.getTask().subscribe((tasks) => (
+      this.tasks = tasks
+    ));
+  }
+  deleteTask(task:Task){
+    this.TaskService.deleteTask(task)
+    .subscribe(
+      () => {
+        this.tasks = this.tasks.filter( (t) =>{
+          return t.id !== task.id
+        })
+      })
+
+  }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder
+    this.TaskService.updateTaskReminder(task).subscribe()
+  }
+  addTask(task:Task){
+    this.TaskService.addTask(task).subscribe((task) => (this.tasks.push(task)))
+  }
+
+}
